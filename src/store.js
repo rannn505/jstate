6697a3072eeadda5ba9c4e401a5500d45245fdef
Ran Cohen <rannn505@outlook.com>
2017-06-10 16:24:04 +0300

    fix getter

--- src/store.js
+++ src/store.js
@@ -3,20 +3,21 @@ let _middlewares = [];
 
 export default {
   get state() {
-    return Object.assign({}, _state);
+    // return Object.assign({}, _state);
+    return JSON.parse(JSON.stringify( _state ));
   },
   setState: (partialState) => {
     if(typeof partialState !== 'object') {
       throw new TypeError('setState() takes an object of state variables to update');
     }
-    _state = Object.assign({}, _state, partialState);
+    _state = {..._state, ...partialState};
   },
   overwriteState: (newState) => {
-    _state = Object.assign({}, newState);
+    _state = {...newState};
   },
   register: (...middlewares) => {
     _middlewares = _middlewares.concat(middlewares);
-    let chain = _middlewares.reduceRight((f, g) => next => f(g(next)))(partialState => _state = Object.assign({}, _state, partialState));
+    let chain = _middlewares.reduceRight((f, g) => next => f(g(next)))(partialState => _state = {..._state, ...partialState});
     module.exports.setState = chain;
     global['$'].setState = chain;
     module.exports.overwriteState = _middlewares.reduceRight((f, g) => {
@@ -24,6 +25,6 @@ export default {
         return f;
       }
       return next => f(g(next));
-    })(newState => _state = Object.assign({}, newState));
+    })(newState => _state = {...newState});
   },
 };
